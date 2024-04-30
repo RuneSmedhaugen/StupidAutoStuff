792f4b8900a566165b17c229633539c8ae133bf6
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2024-04-30 10:10:23 +0200

    Update shopController.js

--- controller/shopController.js
+++ controller/shopController.js
@@ -39,20 +39,6 @@ function buyCharacter(characterName) {
     }
 }
 
-function generateShopCharacters() {
-    const shopCharacters = [];
-    const characters = model.data.characters;
-    let numShopItems = 5;
-
-    for (let i = 0; i < numShopItems; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        shopCharacters.push(characters[randomIndex]);
-        
-    }
-
-    return shopCharacters;
-}
-
 
 function switchToPodiumLeft() {
     const team = model.data.player.team;
@@ -63,7 +49,7 @@ function switchToPodiumLeft() {
 
 function switchToPodiumRight() {
     const team = model.data.player.team;
-    const lastCharacter = team.pop(); // Remove the character from the end
-    team.unshift(lastCharacter); // Add the character to the beginning
-    updateView(); // Update the view to reflect the changes
+    const lastCharacter = team.pop();
+    team.unshift(lastCharacter); 
+    updateView();
 }
